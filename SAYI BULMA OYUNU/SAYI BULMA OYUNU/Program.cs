49db3d3fd110329145cccd146f3e5f032c89e96f
yunusyiilmaz <yns.ylmz1381@gmail.com>
2022-08-12 18:33:54 +0300

    Sayı bulma oyunu
    Sayı bulma oyunu

---
+++ SAYI BULMA OYUNU/SAYI BULMA OYUNU/Program.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SAYI_BULMA_OYUNU
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int sayac = 0;
+            int sayi;
+            Random değer=new Random();
+           sayi = değer.Next(1, 100);
+            int tahmin =0;
+            while(sayac<5)
+            {
+                Console.Write("SAYIYI TAHMİN EDİNİZ :");
+                tahmin=Convert.ToInt32(Console.ReadLine());
+                
+                Console.WriteLine();
+                if (tahmin < sayi)
+                {
+                    Console.WriteLine("daha büyük bir değer giriniz\n");
+                }
+                else if (tahmin > sayi)
+                {
+                    if (tahmin != sayi)
+                    {
+
+                        if (sayac == 4)
+                        {
+                            Console.WriteLine("sayıyı bilemediniz sayı :" + sayi);
+                            break;
+                        }
+                        Console.WriteLine("daha küçük bir sayı girin\n");                       
+                    }
+                }
+            
+                else if (tahmin == sayi)
+                {
+                    Console.WriteLine("tebrikler.");
+                    break;
+                }
+                sayac++;
+            }
+            
+            Console.ReadKey(); 
+
+        }
+    }
+}
